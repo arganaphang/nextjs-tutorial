ad2180d53bcfa90e1a4ae85ebef268871bb19d14
Argana Phangquestian <arganaphangquestian@gmail.com>
2024-03-18 22:02:09 +0700

    update: into

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+	return (
+		<div className="w-full min-h-screen flex justify-center items-center">
+			<div>
+				<img
+					className="w-48 h-48 rounded-full"
+					src="https://mighty.tools/mockmind-api/content/human/57.jpg"
+					alt="John Doe"
+				/>
+				<h1 className="text-center">John Doe</h1>
+			</div>
+		</div>
+	);
+}
